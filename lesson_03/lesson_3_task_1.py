2ef0748f8994337a60f96766e11c1b7bd99236a7
vaspupiy <vaspupiy@mail.ru>
2020-07-14 00:45:05 +0300

    Идея решения первой задачи

--- lesson_03/lesson_3_task_1.py
+++ lesson_03/lesson_3_task_1.py
@@ -1,1 +1,19 @@
-"""Первое задание"""
+def my_div(dividend, divider):
+    """
+    функция выполняет деление двух чисел и возвращает результат деления.
+    В функции предусмотрена обработка ситуации деления на ноль.
+    """
+
+    try:
+        dividend, divider = int(dividend), int(divider)
+    except ValueError:
+        return "Ошибка ввода"
+
+    try:
+        result = dividend / divider
+        return result
+    except ZeroDivisionError:
+        return "В этой стране на ноль делить нельзя"
+
+
+print(my_div(5, 0))
