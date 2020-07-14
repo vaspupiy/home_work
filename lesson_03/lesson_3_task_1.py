260039bd9b22856aeaf037cca523843305fb40ff
vaspupiy <vaspupiy@mail.ru>
2020-07-14 19:50:02 +0300

    lesson_3_task_1

--- lesson_03/lesson_3_task_1.py
+++ lesson_03/lesson_3_task_1.py
@@ -3,12 +3,6 @@ def my_div(dividend, divider):
     функция выполняет деление двух чисел и возвращает результат деления.
     В функции предусмотрена обработка ситуации деления на ноль.
     """
-
-    try:
-        dividend, divider = int(dividend), int(divider)
-    except ValueError:
-        return "Ошибка ввода"
-
     try:
         result = dividend / divider
         return result
@@ -16,4 +10,17 @@ def my_div(dividend, divider):
         return "В этой стране на ноль делить нельзя"
 
 
-print(my_div(5, 0))
+def my_int(string):
+    try:
+        string = int(string)
+        return string
+    except ValueError:
+        print("Ошибка ввода. Ожидается ввод целого числа. Повторите ввод: ")
+        return my_int(input())
+
+
+d_nd = my_int(input("Ведите делимое: "))
+d_er = my_int(input("Ведите делитель: "))
+
+print(my_div(d_nd, d_er))
+
