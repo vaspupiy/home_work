04109c2c0b3c9e223aaa678f1053dc77fef5a5ec
vaspupiy <vaspupiy@mail.ru>
2020-07-14 20:55:04 +0300

    lesson_3_task_3

---
+++ lesson_03/lesson_3_task_3.py
@@ -0,0 +1,17 @@
+def my_func(arg1, arg2, arg3):
+    """Функция принимает три аргумента, и возвращает сумму наибольших двух аргументов"""
+    my_l = [arg1, arg2, arg3]
+    my_l.remove(min(my_l))
+    return sum(my_l)
+
+
+def test():
+    print(f"test_1: {my_func(0, 0, 0) == 0}")
+    print(f"test_2: {my_func(-1, 0, 1) == 1}")
+    print(f"test_1: {my_func(1, 2, 3) == 5}")
+    print(f"test_1: {my_func(-1, -2, -3) == -3}")
+    print(f"test_1: {my_func(1, 1, 2) == 3}")
+    print(f"test_1: {my_func(1, 2, 2) == 4}")
+
+
+test()
